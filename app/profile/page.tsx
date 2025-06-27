b12e06040704c5227f1e14c619eee5c6b471b9cf
Nik Gospodinov <nikola96gospodinov@gmail.com>
2025-06-27 09:25:20 +0100

    feat: redirect when not logged in

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,26 @@
 import { UserProfileForm } from "@/components/forms/user-profile-form/user-profile-form.component";
+import { Spinner } from "@/components/ui/spinner";
+import { useFetchUser } from "@/services/auth/fetch-user";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
+  const { replace } = useRouter();
+  const { data: user, isLoading: isUserLoading } = useFetchUser();
+
+  useEffect(() => {
+    if (!user && !isUserLoading) {
+      replace("/auth");
+    }
+  }, [user, isUserLoading, replace]);
+
+  if (isUserLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner />
+      </div>
+    );
+  }
+
   return <UserProfileForm />;
 }
