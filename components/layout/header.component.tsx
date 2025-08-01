2b3b3161a7b48fb427a4f796c8591224ed8a4359
Nik Gospodinov <nikola96gospodinov@gmail.com>
2025-08-01 13:37:23 +0100

    feat: rename of the actions

--- components/layout/header.component.tsx
+++ components/layout/header.component.tsx
@@ -24,7 +24,7 @@ export const Header = () => {
             <Logo />
           </Link>
           <div className="flex items-center gap-4">
-            <AuthActions />
+            <ProfileActions />
           </div>
         </div>
       </div>
@@ -32,7 +32,7 @@ export const Header = () => {
   );
 };
 
-const AuthActions = () => {
+const ProfileActions = () => {
   const { data: user, isLoading: isUserLoading } = useFetchUser();
   const { mutate: signOut, isPending: isSigningOut } = useSignOut();
   const router = useRouter();
